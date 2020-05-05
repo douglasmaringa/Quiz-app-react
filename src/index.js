e030ac69851ac85c3e1271d8d890c7d0d31e1ef0
douglasmaring <douglasmaringa4@gmail.com>
2020-05-05 23:43:45 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDom from "react-dom"
+import App from "./app"
+
+function MyApp(){
+	return(
+		<div>
+			<App/>
+		</div>
+	)
+}
+
+
+ReactDom.render(<MyApp/>, document.getElementById("root"))
